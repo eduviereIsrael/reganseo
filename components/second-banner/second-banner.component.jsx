876cb8ab142f2c8544bfd969095b7ea5dcbca305
Eduviere Israel <eduviereisrael@gmail.com>
2023-05-04 12:21:15 +0200

    Second banner done?

--- components/second-banner/second-banner.component.jsx
+++ components/second-banner/second-banner.component.jsx
@@ -7,7 +7,7 @@ const SecondBanner = () => {
   return (
     <BannerSection>
         <Banner>
-            <motion.h2 initial={{opacity: 0, y: 15}} whileInView={{opacity: 1, y: 0}} transition={{duration: 0.5, delay: 0.2}} >BECOME AN AUTHORITY <br/> IN YOUR NICHE</motion.h2>
+            <motion.h2 initial={{opacity: 0, y: 15}} whileInView={{opacity: 1, y: 0}} transition={{duration: 0.5, delay: 0.2}} >OUR SERVICES <br/> KEEPS ON DELIVERING</motion.h2>
             <motion.p initial={{opacity: 0, y: 15}} whileInView={{opacity: 1, y: 0}} transition={{duration: 0.5, delay: 0.2}} >With Search Engine Optimization, we aim to programmatically place you on the top results on google for search queries relating to your niche, service or product. </motion.p>
             <motion.p initial={{opacity: 0, y: 15}} whileInView={{opacity: 1, y: 0}} transition={{duration: 0.5, delay: 0.2}}>According to google themselves, the first page of search results gets 99% of all the traffic.</motion.p>
             <motion.span initial={{opacity: 0, y: 15}} whileInView={{opacity: 1, y: 0}} transition={{duration: 0.5, delay: 0.2}}>
